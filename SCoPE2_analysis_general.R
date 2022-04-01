7dd52d242e500e9a543144dc6e3f82e7ce207e99
Andrew-Leduc <62680125+Andrew-Leduc@users.noreply.github.com>
2022-04-01 09:42:13 -0400

    code preprint

--- SCoPE2_analysis_general.R
+++ SCoPE2_analysis_general.R
@@ -1,4 +1,4 @@
-source("functions_parameters.R")
+source("/Users/andrewleduc/Desktop/GitHub/senescence/functions_parameters.R")
 
 # User specific
 
@@ -18,27 +18,26 @@ genes <- read.csv('genes.csv',header = F)
 
 # For converting between uniprot and gene name
 
-convert_mouse <- read.fasta('uniprotMusmusculusSP.fasta',set.attributes = T,whole.header = T)
-convert_mouse <- names(convert_mouse)
-parse_row<-grep("GN=",convert_mouse, fixed=T)
-split_prot<-str_split(convert_mouse[parse_row], pattern = fixed("GN="))
-gene<-unlist(split_prot)[seq(2,2*length(split_prot),2)]
-prot <- unlist(split_prot)[seq(1,2*length(split_prot),2)]
-prot_parse <- grep("|",prot, fixed=T)
-gene_parse <- grep(" ",gene, fixed=T)
-split_gene<-str_split(gene[parse_row], pattern = fixed(" "))
-split_gene<-unlist(split_gene)[seq(1,3*length(split_gene),3)]
-split_prot<-str_split(prot[parse_row], pattern = fixed("|"))
-split_prot<-unlist(split_prot)[seq(2,3*length(split_prot),3)]
-convert_mouse  <- as.data.frame(cbind(split_prot,split_gene))
-convert_mouse$split_gene <- toupper(convert_mouse$split_gene)
+# convert_mouse <- read.fasta('uniprotMusmusculusSP.fasta',set.attributes = T,whole.header = T)
+# convert_mouse <- names(convert_mouse)
+# parse_row<-grep("GN=",convert_mouse, fixed=T)
+# split_prot<-str_split(convert_mouse[parse_row], pattern = fixed("GN="))
+# gene<-unlist(split_prot)[seq(2,2*length(split_prot),2)]
+# prot <- unlist(split_prot)[seq(1,2*length(split_prot),2)]
+# prot_parse <- grep("|",prot, fixed=T)
+# gene_parse <- grep(" ",gene, fixed=T)
+# split_gene<-str_split(gene[parse_row], pattern = fixed(" "))
+# split_gene<-unlist(split_gene)[seq(1,3*length(split_gene),3)]
+# split_prot<-str_split(prot[parse_row], pattern = fixed("|"))
+# split_prot<-unlist(split_prot)[seq(2,3*length(split_prot),3)]
+# convert_mouse  <- as.data.frame(cbind(split_prot,split_gene))
+# convert_mouse$split_gene <- toupper(convert_mouse$split_gene)
 
 
 # Load raw data 
 
-ev<-read.delim("evidence.txt", header = TRUE)
+ev<-read.delim("/Volumes/GoogleDrive/My Drive/MS/Collaborators/Senescence/DART/ev_updated.txt", header = TRUE)
 
-  
 #Here just cleaning up protein names from the fasta to uniprot accession (may not be neccisary)
 # Parse sp|Q00000|HUMAN_XXX into just Uniprot accession: Q00000  
   
@@ -59,9 +58,9 @@ ev<-read.delim("evidence.txt", header = TRUE)
   
 # Load experimental design and batches
   
-  design<-read.csv("annotation_18plex.csv")
+  design<-read.csv("/Users/andrewleduc/Desktop/GitHub/senescence/annotation_18plex.csv")
 
-  batch<-read.csv("batch.csv")
+  batch<-read.csv("/Users/andrewleduc/Desktop/GitHub/senescence/batch.csv")
 
   # Attach batch data to protein data
   ev[,colnames(batch)[-1]]<-NA
@@ -89,6 +88,7 @@ ri.index<-which(colnames(ev)%in%paste0("Reporter.intensity.",1:18))
 # Make sure all runs are described in design, if not, print and remove them:
 not.described<-unique(ev$Raw.file)[ !unique(ev$Raw.file) %in% paste0(design$Set) ]
 ev<-ev[!ev$Raw.file%in%not.described,]
+unique(ev$Raw.file)
 
 # Filter out reverse hits, contaminants, and contaminated spectra...
 ev<-ev[-which(ev$Reverse=="+"),]
@@ -101,16 +101,18 @@ ev<-ev[ev$PIF>0.8,]
 # Remove peptides that are more the 10% the intensity of the carrier in the single cell runs (only)
 ev<-as.data.frame(ev)
 ev$mrri<-0
-ev$mrri <- rowMeans(ev[, ri.index[4:length(ri.index)]] / ev[, ri.index[1]], na.rm = T)
-ev<-ev[ev$mrri < 0.01, ]
+ev$mrri <- rowMeans(ev[, ri.index[6:length(ri.index)]] / ev[, ri.index[1]], na.rm = T)
+ev<-ev[ev$mrri < 0.05, ]
+
+#ev <- ev %>% filter(Intensity > median(ev$Intensity,na.rm = T))
 
 
 # Filter by PEP or FDR: CHOOSE ONE
 
-# ev<-ev[ev$dart_PEP<0.02, ]
-#ev<-ev[ev$PEP<0.02, ]
+ev<-ev[ev$dart_qval<0.01, ]
+#ev<-ev[ev$PEP<0.03, ]
 # ev<-ev[calc_fdr(ev$dart_PEP)<0.01, ]
-ev<-ev[calc_fdr(ev$PEP)<0.01, ]
+#ev<-ev[calc_fdr(ev$PEP)<0.01, ]
 
 
 
@@ -134,6 +136,7 @@ ev.melt<-melt(ev[0, c("Raw.file","modseq","Leading.razor.protein","lcbatch","sor
 colnames(ev.melt)<-c("Raw.file","sequence","protein","lcbatch","sortday","digest","celltype","quantitation")
 
 
+
 # Record mapping of cell type to Channel:
 ct.v<-c()
 qt.v<-c()
@@ -143,7 +146,7 @@ unique.id.numeric<-1:length(ri.index)
 unique.id<-paste0("i",unique.id.numeric)
 
 RI_keep<-ri.index
-
+ev <- ev %>% filter(is.na(Raw.file)==F)
 # Give each sample a unique identifier
 for(X in unique(ev$Raw.file)){
 
@@ -171,7 +174,8 @@ for(X in unique(ev$Raw.file)){
 
 }
 
-c2q<-data.frame(ct.v, qt.v); colnames(c2q)<-c("celltype","channel")
+c2q<-data.frame(ct.v, qt.v); colnames(c2q)<-c("id","channel")
+c2q$id <- as.character(c2q$id )
 
 # Grab the unique number associate to each and every cell, carrier channel, and empty channel
 ev.melt$id<-unlist(strsplit(as.character(ev.melt$celltype),"-"))[seq(2,length(unlist(strsplit(as.character(ev.melt$celltype),"-"))),2)]
@@ -184,6 +188,7 @@ ev.melt<-ev.melt[!is.na(ev.melt$protein), ]
 
 # Create additional meta data matrices
 ev.melt.uniqueID<-remove.duplicates(ev.melt,"id")
+ev.melt.uniqueID <- left_join(ev.melt.uniqueID,c2q, by = 'id')
 ev.melt.pep<-remove.duplicates(ev.melt, c("sequence","protein") )
 
 # Create data frame of peptides x cells, populated by quantitation
@@ -215,48 +220,52 @@ sc.melt<-ev.melt
 
 xd<-as_tibble( sc.melt )
 
-xd <- xd %>% group_by(id) %>% mutate(med_per_c = median(quantitation, na.rm=T)); length(unique(xd$id))
+xd <- xd %>% dplyr::group_by(id) %>% dplyr::mutate(med_per_c = median(quantitation, na.rm=T)); length(unique(xd$id))
 
 length(unique(xd$id))
 
 xd$quantitation[(xd$quantitation)==Inf]<-NA
 xd$quantitation[(xd$quantitation)==0]<-NA
 
-xd <- xd %>% mutate_if(is.factor, as.character)
+xd <- xd %>% dplyr::mutate_if(is.factor, as.character)
 
 xd1 <- xd %>%
-  group_by(id) %>%
-  mutate(norm_q1 = quantitation / median(quantitation, na.rm=T))
+  dplyr::group_by(id) %>%
+  dplyr::mutate(norm_q1 = quantitation / median(quantitation, na.rm=T))
 
 xd2 <- xd1 %>%
-  group_by(sequence, Raw.file) %>%
-  mutate(norm_q = quantitation / median(norm_q1, na.rm=T))
+  dplyr::group_by(sequence, Raw.file) %>%
+  dplyr::mutate(norm_q = quantitation / median(norm_q1, na.rm=T))
 
 xd3<- xd2 %>%
-  filter(celltype%in%c(your_labels))
+  dplyr::filter(celltype%in%c(your_labels))
 
 xd4<- xd3 %>%
-  group_by(protein, id) %>%
+  dplyr::group_by(protein, id) %>%
   mutate(cvq = cv(norm_q))
 
 xd5<- xd4 %>%
-  group_by(protein, id) %>%
-  mutate(cvn = cvna(norm_q))
+  dplyr::group_by(protein, id) %>%
+  dplyr::mutate(cvn = cvna(norm_q))
 
 xd6<- xd5 %>%
   filter(cvn > 2)
 
 
-xd7<-xd6 %>% group_by(id) %>% mutate(cvm=median(cvq, na.rm=T))
+xd7<-xd6 %>% group_by(id) %>% dplyr::mutate(cvm=median(cvq, na.rm=T))
 
 xdf<-xd7
+look <- xdf %>% filter(control == 'ctl')
+look <- look %>% distinct(Raw.file,.keep_all = T)
 
 #print("Number of unique proteins used in calculation:", length(unique(xdf$protein)))
 
+median(xdf$cvm)
 # Filter out variable wells and controls
-cvPar <- 0.46
+cvPar <- 0.47
 sc_kept<-unique( xdf$id[xdf$celltype!=your_control_label & xdf$cvm < cvPar])
 sc0_kept<-unique( xdf$id[xdf$celltype==your_control_label & xdf$cvm > cvPar])
+
 
 # Which wells to keep
 keep_these<-unique( xdf$id)
@@ -276,7 +285,21 @@ xdf$control[xdf$celltype==your_control_label]<-"ctl"
 my_col3<-c( "black", "purple2")
 
 
-
+ggplot(data=xdf, aes(x=cvm,fill=control)) + geom_density(aes( alpha=0.5), adjust=4) + theme_pubr() +
+  scale_fill_manual(values=my_col3[c(1,2)]) + 
+  xlab("Quantification variability") + ylab("Fraction of cells") + rremove("y.ticks") + rremove("y.text") +
+  font("xylab", size=30) +
+  font("x.text", size=20) +
+  coord_cartesian(xlim=c(.0,1))+
+  #xlim(c(-0.15, 0.35)) +
+  # annotate("text", x=0.27, y= 14, label=paste0(scrate,"% single cells passed"), size=8, color=my_col3[c(2)])+
+  # annotate("text", x=0.27, y= 12.5, label=paste0(sc0rate,"% control wells passed"), size=8, color=my_col3[c(1)])+
+  annotate("text", x=0.26, y= 14, label=paste0(length(sc_kept),"cells"), size=10, color=my_col3[c(2)])+
+  annotate("text", x=0.64, y= 12, label=paste0(length(sc0_kept)," Ctr -"), size=10, color=my_col3[c(1)])+
+  annotate("text", x=0.63, y= 14, label=paste0(length(sc_total) -length(sc_kept)," cells"), size=10, color=my_col3[c(2)])+
+  annotate("text", x=0.245, y= 12, label=paste0(length(sc0_total) - length(sc0_kept)," Ctr -"), size=10, color=my_col3[c(1)])+
+  #annotate("text", x=0.25, y= 3, label="Macrophage-like", size=6) +
+  rremove("legend") + geom_vline(xintercept=0.46, lty=2, size=2, color="gray50") + theme(plot.margin = margin(0, 1, 0, 0, "cm"))
 
 
 
@@ -336,7 +359,7 @@ t3m$pep<-rownames(t3)
 t3m$prot <- ev.melt.pep$protein[match(t3m$pep, ev.melt.pep$sequence)]
 t3m<-melt(t3m, variable.names = c("pep", "prot"))
 colnames(t3m) <-c("pep","prot","id","quantitation")
-t3m2<- t3m %>% group_by(prot, id) %>% summarize(qp = median(quantitation, na.rm=T))
+t3m2<- t3m %>% group_by(prot, id) %>% dplyr::summarize(qp = median(quantitation, na.rm=T))
 t4m<-dcast(t3m2, prot ~ id, value.var = "qp", fill=NA)
 t4<-as.matrix(t4m[,-1]); row.names(t4)<-t4m[,1]
 hist(c(t4), breaks=b.t, xlim=xlim.t)
@@ -359,37 +382,22 @@ dim(sc.imp)
 
 sc.imp[(is.na(sc.imp))]<-0
 
-# Batch correction with ComBat
-batch.N<-table(ev.melt.uniqueID$Raw.file[ev.melt.uniqueID$id%in%colnames(sc.imp)])
-sc.imp<-sc.imp[,!colnames(sc.imp)%in%ev.melt.uniqueID$id[ev.melt.uniqueID$Raw.file%in%names(batch.N)[batch.N==1]]]
-
-# ### Uncomment if ComBat fails to execute because there are proteins that don't vary within one or more experiments
-# # Remove proteins with no variance
-# batch.covs <- ev.melt.uniqueID$Raw.file[match(colnames(sc.imp), ev.melt.uniqueID$id)]
-# rr<-c()
-# rawx<-c()
-# for(Y in unique(batch.covs)){
-#   
-#   xt<-sc.imp[,batch.covs%in%Y]
-#   
-#   vt<-rowVars(xt)
-#   
-#   rawx<-c(rawx, rep(Y, length(which(vt==0))))
-#   
-#   rr<-c(rr, which(vt==0) )
-#   
-# }
-# 
-# sc.imp<-sc.imp[-rr,]; dim(sc.imp)
-# ### ^^ Uncomment if ComBat fails to execute because there are proteins that don't vary within one or more experiments
 
 
 # Define the batches and model:
 batch.covs <- ev.melt.uniqueID$Raw.file[match(colnames(sc.imp), ev.melt.uniqueID$id)]
 mod<-data.frame(ev.melt.uniqueID$celltype[match(colnames(sc.imp), ev.melt.uniqueID$id)]); colnames(mod)<-"celltype"
-mod<-model.matrix(~as.factor(celltype), data=mod)
+#mod<-model.matrix(~as.factor(celltype), data=mod)
 
+batch.covsRI <- ev.melt.uniqueID$channel[match(colnames(sc.imp), ev.melt.uniqueID$id)]
+
+#Cell Type Info Labels
+#mod<-data.frame(ev.melt.uniqueID$celltype[match(colnames(sc.imp), ev.melt.uniqueID$id)]); colnames(mod)<-"celltype"
+mod<-model.matrix(~as.factor(celltype), data=mod)
 matrix.sc.batch <- ComBat(sc.imp, batch=batch.covs)
+
+#matrix.sc.batch <- removeBatchEffect(sc.imp, batch=factor(batch.covs), batch2=factor(batch.covsRI))#,design=mod)
+
 t6<-matrix.sc.batch
 
 # visual sanity checks post-imputation:
@@ -398,6 +406,8 @@ hist(c(t6), breaks=b.t, xlim=xlim.t)
 
 par(mfrow=c(1,1))
 
+t7 <- cr_norm_log(t6)
+t7[is.na(t4b)==T] <- NA
 
 
 
@@ -411,8 +421,7 @@ PCy<-"PC2"
 # Data to use:
 
 
-mat.sc.imp<-cr_norm_log(matrix.sc.batch)
-
+mat.sc.imp<-cr_norm_log(t6)
 # Dot product of each protein correlation vector with itself
 r1<-cor(t(matrix.sc.batch))
 rsum<-rowSums(r1^2)
@@ -421,7 +430,7 @@ rsum<-rowSums(r1^2)
 
 X.m <- mat.sc.imp
 X.m <- diag(rsum) %*%  X.m
-pca.imp.cor <- cor(X.m)
+pca.imp.cor <- cor(X.m,use = 'pairwise.complete.obs')
 
 # PCA
 sc.pca<-eigen(pca.imp.cor)
@@ -439,87 +448,140 @@ pca.melt <- melt(scx); colnames(pca.melt)<-c("id","pc","value")
 # Re map ...
 pca.display <- dcast(pca.melt, id ~ pc, value.var = "value", fill=NA)
 pca.display <- pca.display %>% left_join(ev.melt.uniqueID,by = 'id')
-c2q$celltype <- as.character(c2q$celltype)
-colnames(c2q)[1]<- 'id'
-# Display 
 
-#color = 'AT2'
 
-ggscatter(pca.display, x =PCx, y = PCy ,  size = 4, alpha=0.5) +
+ggscatter(pca.display,x =PCx, y = PCy ,  size = 4, alpha=0.5) +
   xlab(paste0(PCx,"  (", round(percent_var[1],0),"%)")) +
   ylab(paste0(PCy,"  (", round(percent_var[2],0),"%)")) +
   font("ylab",size=30) +
   font("xlab",size=30) +
   font("xy.text", size=20) +
-  rremove("legend") +
+  #rremove("legend") +
   #scale_color_manual(values = my_colors[2:3]) +
   #annotate("text", x=-0.025, y=0.21,label=your_control_label, color=my_colors[2], size=10)  +
   #annotate("text", x=0.03, y=0.21, label="Monocyte", color=my_colors[3], size=10) +
-  annotate("text", x=0.05-0.02, y=0.17, label=paste0(dim(mat.sc.imp)[1], " proteins"), size=8) +
-  annotate("text", x=0.062-0.03, y=0.14, label=paste0(dim(mat.sc.imp)[2], " cells"), size=8) +
+  #annotate("text", x=-.03, y=0.12, label=paste0(dim(mat.sc.imp)[1], " proteins"), size=8) +
+  #annotate("text", x=-0.03, y=0.07, label=paste0(dim(mat.sc.imp)[2], " cells"), size=8) +
   scale_color_gradient2(midpoint = 0, low = "blue", mid = "white",
-                        high = "red",name = '') + theme(plot.title = element_text(size = 20))
+                       high = "red") #+ theme(plot.title = element_text(size = 20))
+
+
+c1 <- pca.display %>% filter(PC1 > .02)
+c1 <- c1 %>% filter(PC2 < 0)
+c2 <- pca.display %>% filter(PC1 > .02)
+c2 <- c2 %>% filter(PC2 > 0)
+c3 <- pca.display %>% filter(PC1 < .0) # this is the AT1 AT2 basel population
+
+
+### Here we are getting things in an accessible format for doing differential espression or Gene set enrichment
+
+data <- melt(mat.sc.imp)
+data$Condition <- NA
+data$Condition[data$Var2 %in% c1$id] <- 'c1'
+data$Condition[data$Var2 %in% c2$id] <- 'c2'
+data$Condition[data$Var2 %in% c3$id] <- 'c3'
+data <-data %>% filter(is.na(Condition)==F)
+
+
+colnames(data)<- c('Protein','SampID','Intensity','Condition')
+
+GSEA_out <- Population_GSEA(data,GO_db,3,1,converter_df)
+GSEA_out <- GSEA_out %>% filter(is.na(pVal)==F)
+GSEA_out$qval <- p.adjust(GSEA_out$pVal,method = 'BH')
+GSEA_out <- GSEA_out %>% filter(qval < .01)
+GSEA_out_ <-GSEA_out %>% filter(numberOfMatches > 3)
+
+
+
+## Diff protein on on AT1, AT2, Basel
+
+##### <- make PCA on subcluster, this involves re-normalizing data
+
+# filtering for genes with 20 percent present values 
+t7_filt <- filt.mat.rc(t7,.8,.99)
+dim(t7_filt)
+
+#select for C3 population (basel,AT1,AT2)
+t7_filt <- as.data.frame(t7_filt) %>% dplyr::select(c3$id)
+
+## Here is the renormalization
+t7_filt <- cr_norm_log(as.matrix(t7_filt))
+
+X.m <- as.matrix(t7_filt)
+pca.imp.cor <- cor(X.m,use = 'pairwise.complete.obs')
+sc.pca<-eigen(pca.imp.cor)
+scx<-as.data.frame(sc.pca$vectors)
+colnames(scx)<-paste0("PC",1:ncol(scx))
+scx$cells<-colnames(pca.imp.cor)
+
+# Percent of variance explained by each principle component
+pca_var <- sc.pca$values
+percent_var<- pca_var/sum(pca_var)*100
+plot(1:length(percent_var), percent_var, xlab="PC", ylab="% of variance explained")
+
+# Map meta data
+pca.melt <- melt(scx); colnames(pca.melt)<-c("id","pc","value")
+# Re map ...
+pca.display <- dcast(pca.melt, id ~ pc, value.var = "value", fill=NA)
+pca.display <- pca.display %>% left_join(ev.melt.uniqueID,by = 'id')
+
+ggscatter(pca.display,x =PCx,y = PCy ,  size = 4, alpha=0.5) +
+  xlab(paste0(PCx,"  (", round(percent_var[1],0),"%)")) +
+  ylab(paste0(PCy,"  (", round(percent_var[2],0),"%)")) +
+  font("ylab",size=30) +
+  font("xlab",size=30) +
+  font("xy.text", size=20) 
+  #rremove("legend") +
+  #scale_color_manual(values = my_colors[2:3]) +
+  #annotate("text", x=-0.025, y=0.21,label=your_control_label, color=my_colors[2], size=10)  +
+  #annotate("text", x=0.03, y=0.21, label="Monocyte", color=my_colors[3], size=10) +
+  #annotate("text", x=-.03, y=0.12, label=paste0(dim(mat.sc.imp)[1], " proteins"), size=8) +
+  #annotate("text", x=-0.03, y=0.07, label=paste0(dim(mat.sc.imp)[2], " cells"), size=8) +
+  #scale_color_gradient2(midpoint = 0, low = "blue", mid = "white",
+  #                      high = "red") #+ theme(plot.title = element_text(size = 20))
+
+
+### Define new clusters manually
+
+c1 <- pca.display %>% filter(PC1 < 0)
+c1 <- c1 %>% filter(PC2 > -.025)
+c2 <- pca.display %>% filter(PC1 > .0)
+c3 <- pca.display %>% filter(PC1 < .0) # this is the AT1 AT2 basel population
+c3 <- c3 %>% filter(PC2 < -.025)
+
+#colnames(converter_df) <- c('mouse_uni','shared_gene','human_uni')
+#write.csv(converter_df,'/Users/andrewleduc/Desktop/convert_uniprot_mouse_human.csv')
+
+
+#again get data in format that allows us to easily do diff exp
+
+data <- melt(t7_filt)
+data$Condition <- NA
+data$Condition[data$Var2 %in% c1$id] <- 'c1'
+data$Condition[data$Var2 %in% c2$id] <- 'c2'
+data$Condition[data$Var2 %in% c3$id] <- 'c3'
+data <-data %>% filter(is.na(Condition)==F)
+
+
+colnames(data)<- c('Protein','SampID','Intensity','Condition')
+
+converter_df <- read.csv('convert_uniprot_mouse_human.csv')
+GO_db <- read.delim('GOA_db.txt',sep = " ")
+
+
+## GSEA code
+GSEA_out <- Population_GSEA(data,GO_db,3,1,converter_df)
+GSEA_out <- GSEA_out %>% filter(is.na(pVal)==F)
+GSEA_out$qval <- p.adjust(GSEA_out$pVal,method = 'BH')
+GSEA_out <- GSEA_out %>% filter(qval < .01)
+GSEA_out_ <-GSEA_out %>% filter(numberOfMatches > 3)
+
+
+
+diff_prot_output <- data.frame(GO_term = character(), pVal = numeric(),numberOfMatches= numeric(),fractionOfDB_Observed = numeric(),stringsAsFactors = FALSE,
+                          Cond1med_int = numeric(),Cond2med_int = numeric(),Cond3med_int = numeric())
 
 
 
 
-####### correlating protiens to PC1 and PC2
-protein <- c()
-PC1_cor <- c()
-PC2_cor <- c()
-for(i in 1:nrow(t4b)){
-  protein <- c(protein,rownames(t4b)[i])
-  PC1_cor <- c(PC1_cor, cor(t4b[i,],pca.display$PC1,use = "pairwise.complete.obs"))
-  PC2_cor <- c(PC2_cor, cor(t4b[i,],pca.display$PC2,use = "pairwise.complete.obs"))
-}
-
-PC_Cor <- as.data.frame(protein)
-PC_Cor$pc1 <- PC1_cor
-PC_Cor$pc2 <- PC2_cor
-
-ev <- ev %>% filter(Leading.razor.protein == 'Q8R1H0')
-AT1 <- convert_mouse %>% filter(split_gene %in% toupper(AT1))
-AT1 <- t4b[rownames(t4b) %in% AT1$split_prot,]
-
-AT2 <- convert_mouse %>% filter(split_gene %in% toupper(AT2))
-AT2$split_prot%in% rownames(t4b) 
-AT2 <- t4b[rownames(t4b) %in% AT2$split_prot,]
-
-pca.display$AT2 <- AT2
-
-mat.sc.imp <- as.data.frame(mat.sc.imp) #,convert_mouse$split_prot))
-mat.sc.imp$split_prot <- rownames(mat.sc.imp)
-mat.sc.imp <- mat.sc.imp %>% left_join(convert_mouse,by = 'split_prot')
-mat.sc.imp <- mat.sc.imp %>% filter(is.na(split_gene) ==F)
-rownames(mat.sc.imp) <- mat.sc.imp$split_gene
-mat.sc.imp$split_prot <- NULL
-mat.sc.imp <- as.matrix(mat.sc.imp)
-
-write.csv(mat.sc.imp,file = '/Users/andrewleduc/Desktop/geneXsc.csv')
-
-Basal <- convert_mouse %>% filter(split_gene %in% toupper(genes$V1))
-Club <- convert_mouse %>% filter(split_gene %in% toupper(genes$V2))
-Ciliated <- convert_mouse %>% filter(split_gene %in% toupper(genes$V3))
-Tuft <- convert_mouse %>% filter(split_gene %in% toupper(genes$V4))
-Neuroendocrine <- convert_mouse %>% filter(split_gene %in% toupper(genes$V5))
-Ionocyte <- convert_mouse %>% filter(split_gene %in% toupper(genes$V6))
-
-
-AT2 <- t4b[rownames(t4b) %in% Ionocyte$split_prot,]
-AT2 <- t4b[rownames(t4b) =='P14869',]
-
-s <-cor(t(AT2),use = 'pairwise.complete.obs')
-s <- s[lower.tri(s)]
-hist(s)
-
-median(s)
-
-look <- a %>% filter(a %in% rownames(AT2))
-
-
-#'Selenbp1','Gsto1','Sftpa1','Tst','Scgb1a1'
-see <- convert_mouse %>% filter(split_prot %in% rownames(AT2))
-
-see$split_gene
-pca.display$AT2 <- colMeans(AT2,na.rm = T)
 
